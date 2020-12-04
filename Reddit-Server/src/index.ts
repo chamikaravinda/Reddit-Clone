64b24ff044a208bb2a649123e6b199b3c4383c99
Chamika Ravinda <chamikaravinda@gmail.com>
2020-12-04 14:24:34 +0530

    Project Setup for back end

--- Reddit-Server/src/index.ts
+++ Reddit-Server/src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello world 2");
+import {MikroORM} from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+
+
+const main = async() =>{
+    const orm = await MikroORM.init({
+        entities:[Post],
+        dbName:'reddit',
+        type:'postgresql',
+        debug:!__prod__,
+    });
+
+    orm.em.create(Post,{title:"My first post"});
+}
+
+
+main();
+
